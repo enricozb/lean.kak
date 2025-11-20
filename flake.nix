7750cb54b2d5684f4f24c9e2428f71d5bd5011c1
Enrico Zandomeni Borba <enricozb@gmail.com>
2025-11-20 21:18:43 +0100

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "lean.kak";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.fenix = {
+    url = "github:nix-community/fenix/monthly";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-9se7PrPgIQRzVmopn9PtbQ292bfnFP+h/mpCFEHcgwY=";
+        };
+        rust-platform = pkgs.makeRustPlatform {
+          rustc = rust-toolchain;
+          cargo = rust-toolchain;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+
+          packages = [
+            rust-toolchain
+          ];
+        };
+
+        formatter = pkgs.nixfmt;
+      }
+    );
+}
